5079fc1a28c5699e14c1e7b66bf99ac26205d1f4
Katie Jeffree <k.jeffree@gmail.com>
2018-10-31 10:25:57 +0000

    server side working

---
+++ server/db/seeds.js
@@ -0,0 +1,20 @@
+use bucketList;
+db.dropDatabase();
+
+db.activities.insertMany([
+  {
+    activity: "Defeat Voldermort",
+    type: "Recreational",
+    date: "2018-12-25"
+  },
+  {
+    activity: "Join the Quidditch team",
+    type: "Sport",
+    date: "2018-11-04"
+  },
+  {
+    activity: "Ride a Hippogriff",
+    type: "Outdoor",
+    date: "2019-01-01"
+  }
+]);
